f861d20ebd588005c19a20d547e28a50e4d4368e
L F <franco100lucas@gmail.com>
2024-04-21 09:56:45 -0300

    feat: Implementing navigation to login and register using Stack navigation

--- src/routes/navigation.js
+++ src/routes/navigation.js
@@ -2,15 +2,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from '../paginas/Home'
 import Login from '../paginas/Login'
+import Cadastro from '../paginas/Cadastro'
 
 const Stack = createStackNavigator()
 
 export default function Navigation() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home} />
+            <Stack.Navigator initialRouteName="Home"
+            screenOptions={{
+                headerTitle: "", //Titulo do cabeçalho das Stacks
+                headerShown: false // Mostrar cabeçalho
+            }}>
+                <Stack.Screen name="Home"
+                 component={Home}/>
                 <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Cadastro" component={Cadastro} />
             </Stack.Navigator>
         </NavigationContainer>
     )
